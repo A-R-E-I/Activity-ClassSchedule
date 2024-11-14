825d264175615be93b0dc1d31f5dee6e35a68ee3
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-11-14 10:20:45 -0500

    Add files via upload

--- ClassSchedule2.js
+++ ClassSchedule2.js
@@ -4,10 +4,20 @@ function GetInfo()
 {
 	firstname = localStorage.getItem("firstname");
 	lastname = localStorage.getItem("lastname");
-	Osis = localStorage.getItem("OSIS");
 	fullname = lastname + ", " + firstname;
+	Osis = localStorage.getItem("OSIS");
+	Gradelevel = localStorage.getItem("lvl");
+	OffclassF = localStorage.getItem("OfficialF");
+	OffclassNum = localStorage.getItem("Officialnum");
+	OffclassL = localStorage.getItem("OfficialL");
+	Officialclass = OffclassF  + OffclassNum + OffclassL;
+	CounsF = localStorage.getItem("CounsFN");
+	CounsL = localStorage.getItem("CounsLN");
+	CounselorN = CounsL + " " + CounsF;
 	
 	document.getElementById("lblfullname").textContent = "Name: " + fullname;
-	document.getElementById("lblOsis").textContent = Osis;
+	document.getElementById("lblOsis").textContent = "Student ID: " + Osis;
+	document.getElementById("Glvl").textContent = "Grade level: " + Gradelevel;
+	document.getElementById("OffClass").textContent = "Official class: " + Officialclass;
+	document.getElementById("Counselor").textContent = "Counselor: " + CounselorN;
 }
-
